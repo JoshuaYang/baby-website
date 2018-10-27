3f71f6f1f39ad64fa1d765e4a3ebbabf2f8ad253
ヨシュア <yss901025@gmail.com>
2018-10-27 15:37:13 +0800

    init

---
+++ pages/timeline.vue
@@ -0,0 +1,152 @@
+<template lang="html">
+  <div class="page-baby-timeline">
+    <DatePicker
+      type="date"
+      placeholder="出生日期"
+      style="width: 200px"
+    />
+
+    <Alert>An info prompt</Alert>
+
+    <Timeline pending>
+      <TimelineItem>
+        <p class="time">
+          2018-12-12
+          <Tag color="primary">1月龄</Tag>
+        </p>
+
+        <div class="content">
+          <Badge
+            status="success"
+            text="打针1"
+          />
+
+          <Badge
+            status="success"
+            text="打针2"
+          />
+
+          <Badge
+            status="processing"
+            text="打针3"
+          />
+        </div>
+      </TimelineItem>
+
+      <TimelineItem color="green">
+        <Icon
+          slot="dot"
+          type="ios-woman"
+        />
+
+        <p class="time">
+          today
+        </p>
+      </TimelineItem>
+
+      <TimelineItem>
+        <p class="time">
+          2018-12-12
+          <Tag color="primary">1月龄</Tag>
+        </p>
+
+        <div class="content">
+          <Badge
+            status="success"
+            text="打针1"
+          />
+
+          <Badge
+            status="success"
+            text="打针2"
+          />
+
+          <Badge
+            status="processing"
+            text="打针3"
+          />
+        </div>
+      </TimelineItem>
+
+      <TimelineItem>
+        <p class="time">
+          2018-12-12
+          <Tag color="primary">1月龄</Tag>
+        </p>
+
+        <div class="content">
+          <Badge
+            status="success"
+            text="打针1"
+          />
+
+          <Badge
+            status="success"
+            text="打针2"
+          />
+
+          <Badge
+            status="processing"
+            text="打针3"
+          />
+        </div>
+      </TimelineItem>
+
+      <TimelineItem>
+        <p class="time">
+          2018-12-12
+          <Tag color="primary">1月龄</Tag>
+        </p>
+
+        <div class="content">
+          <Badge
+            status="success"
+            text="打针1"
+          />
+
+          <Badge
+            status="success"
+            text="打针2"
+          />
+
+          <Badge
+            status="processing"
+            text="打针3"
+          />
+        </div>
+      </TimelineItem>
+
+      <TimelineItem>...</TimelineItem>
+    </Timeline>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.page-baby-timeline {
+    .ivu-timeline {
+        margin-top: 20px;
+
+        .time {
+            font-size: 14px;
+            font-weight: bold;
+
+            .ivu-tag {
+                margin-left: 10px;
+            }
+        }
+        .content {
+            margin-top: 10px;
+            padding-left: 10px;
+
+            .ivu-badge {
+                display: block;
+            }
+        }
+    }
+
+}
+</style>
