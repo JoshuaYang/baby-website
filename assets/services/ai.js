2fd73846070b577dc5fba72bce1f83bebd81d5eb
Joshua Yang <yss901025@gmail.com>
2018-12-27 07:48:33 +0800

    ai

--- assets/services/ai.js
+++ assets/services/ai.js
@@ -2,9 +2,30 @@ import {
   aiPost
 } from '@/assets/utils/aiRequest'
 
-// 獲取時間線列表
+// 文本翻譯
 export async function textTrans (params) {
   return aiPost('/fcgi-bin/nlp/nlp_texttrans', {
+    body: params
+  })
+}
+
+// 智能闲聊
+export async function textChat (params) {
+  return aiPost('/fcgi-bin/nlp/nlp_textchat', {
+    body: params
+  })
+}
+
+// 意圖成分
+export async function wordCom (params) {
+  return aiPost('/fcgi-bin/nlp/nlp_wordcom', {
+    body: params
+  })
+}
+
+// 情感分析
+export async function textPolar (params) {
+  return aiPost('/fcgi-bin/nlp/nlp_textpolar', {
     body: params
   })
 }
--- pages/ai.vue
+++ pages/ai.vue
@@ -1,19 +1,55 @@
 <template>
-  <v-btn @click="testHandler">test</v-btn>
+  <div class="page-ai">
+    <v-text-field v-model="text"></v-text-field>
+    <p>{{transText}}</p>
+    <v-btn @click="textPolarHandler">test</v-btn>
+  </div>
+  
 </template>
 
 <script>
 import {
-  textTrans
-  // getWeather
+  textTrans,
+  textChat,
+  wordCom,
+  textPolar
 } from '@/assets/services/ai'
 
 export default {
+  data () {
+    return {
+      text: '',
+      transText: ''
+    }
+  },
   methods: {
-    testHandler () {
-      textTrans({
-        type: 0,
-        text: '幾點了？'
+    async textTransHandler () {
+      const response = await textTrans({
+        type: 3,
+        text: this.text
+      })
+
+      this.transText = response.data.data.trans_text
+    },
+
+    async textChatHandler () {
+      const response = await textChat({
+        session: 'asdf',
+        question: this.text
+      })
+
+      this.transText = response.data.answer
+    },
+
+    async wordComHandler () {
+      await wordCom({
+        text: this.text
+      })
+    },
+
+    async textPolarHandler () {
+      await textPolar({
+        text: this.text
       })
     }
   }
